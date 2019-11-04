0812e3722be3cbf79e88c83463b046a497b9f8e5
Gabriel Fletcher <gabriel.fletcher@csueastbay.edu>
2019-11-03 22:09:15 -0800

    Big Change

--- js/app.js
+++ js/app.js
@@ -28,3 +28,22 @@ else if (hrs >= 17 && hrs <= 24)
 
 document.getElementById('lblGreetings').innerHTML =
     '<b>' + greet + '</b>';
+
+    
+    // portfolio
+     $('.gallery ul li a').click(function() {
+         var itemID = $(this).attr('href');
+         $('.gallery ul').addClass('item_open');
+         $(itemID).addClass('item_open');
+         return false;
+     });
+     $('.close').click(function() {
+         $('.port, .gallery ul').removeClass('item_open');
+         return false;
+     });
+
+     $(".gallery ul li a").click(function() {
+         $('html, body').animate({
+             scrollTop: parseInt($("#top").offset().top)
+         }, 400);
+     });
